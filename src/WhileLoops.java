1c118fe371cb78660e388be1bb232afea74bc185
Lucy <thomalu@ssfss.org>
2019-11-11 11:44:01 -0500

    Initial commit

---
+++ src/WhileLoops.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class WhileLoops {
+    static Scanner scan = new Scanner(System.in);
+    public static String fromHereToThere(int a, int b){
+        int count = 20;
+        while(count<=30){
+            System.out.println(count+"");
+            count++;
+        }
+    }
+}
