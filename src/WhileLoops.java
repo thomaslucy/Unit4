98beacda495b25cef77ea192637895ad9ac6806a
Lucy <thomalu@ssfss.org>
2019-11-11 12:05:33 -0500

    working on WhileLoops

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -2,11 +2,27 @@ import java.util.Scanner;
 
 public class WhileLoops {
     static Scanner scan = new Scanner(System.in);
-    public static String fromHereToThere(int a, int b){
-        int count = 20;
-        while(count<=30){
-            System.out.println(count+"");
-            count++;
+    public static void fromHereToThere(int a, int b){
+        if (a>b) {
+            System.out.println("Invalid Input");
         }
+        while (a<=b){
+            System.out.println(a);
+            a+=1;
+
+        }
+    }
+
+    public static void factors(int c) {
+        int num = 1;
+    while (num<=c) {
+            num+=1;
+     if (c%num==0)
+         System.out.println(c/num);
+        }
+    }
+    public static void main(String[] args){
+        fromHereToThere(20,30);
+        factors(12);
     }
 }
