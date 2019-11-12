cc33a26a02f846b62a0bcd870bb8ba9ff8172320
Lucy <thomalu@ssfss.org>
2019-11-12 14:14:08 -0500

    working on WhileLoops

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -21,8 +21,26 @@ public class WhileLoops {
          System.out.println(c/num);
         }
     }
+    public static String countPosandNeg(){
+        System.out.println("Enter a positive or negative number or 0 to quit: ");
+        int pcount = 0;
+        int ncount = 0;
+        int num = scan.nextInt();
+        while (num != 0){
+            if (num>0) {
+                pcount += 1;
+            }
+            if (num<0) {
+                ncount += 1;
+            }
+            num = scan.nextInt();
+        }
+        return "There were "+pcount+" positive and "+ncount+" negative numbers.";
+    }
+    public static void findMinAndMax()
     public static void main(String[] args){
         fromHereToThere(20,30);
         factors(12);
+        System.out.println(countPosandNeg());
     }
 }
