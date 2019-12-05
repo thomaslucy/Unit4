dc5d80b67323ff5dead891c29eed7f86136671d4
Lucy <thomalu@ssfss.org>
2019-12-05 12:04:51 -0500

    working on NestedLoops

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -22,16 +22,29 @@ public class NestedLoops {
     public static String flippedTriangle(int n){
         String s = "";
         for (int x = 1; x <= n; x++){
-            for (int y = 1; y <= x; y++)
+            for (int y = x; y <= n; y++)
                 s += "*";
             s += "\n";
         }
         return s;
     }
 
+    public static String multiplicationTable(){
+        String s = "";
+        for (int x = 1; x <= 9; x++) {
+            for (int y = 1; y <= 9; y++)
+                s += x*y + " ";
+                s += "\n";
+        }
+        if (s.length()==1)
+            s += "  ";
+        return s;
+    }
+
     public static void main(String[] args){
         //System.out.println(xSquare());
         //System.out.println(xSquare2(8));
-        System.out.println(flippedTriangle(6));
+        //System.out.println(flippedTriangle(7));
+        System.out.println(multiplicationTable());
     }
 }
